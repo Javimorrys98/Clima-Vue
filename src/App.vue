ef842b15eaa652b9723d7edc3c52f5f5a8251057
Javier Tomé <jtomeroldan@gmail.com>
2024-02-14 09:24:40 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+import Formulario from './components/Formulario.vue';
+import Clima from './components/Clima.vue'
+import Loader from './components/Loader.vue';
+import useClima from './composables/useClima';
+import Alerta from './components/Alerta.vue';
+
+const {obtenerClima, clima, mostrarClima, cargando, error} = useClima()
+
+</script>
+
+<template>
+	<h1 class="titulo">Buscador de clima</h1>
+	<div class="contenedor buscador-clima">
+		<Formulario @obtener-clima="obtenerClima"/>
+		<Loader v-if="cargando"/>
+		<Alerta v-if="error">{{error}}</Alerta>
+		<Clima v-if="mostrarClima" :clima="clima"/>
+	</div>
+</template>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { reactive, ref } from 'vue'
+import Alerta from '../components/Alerta.vue'
+
+const paises = [
+    { codigo: 'US', nombre: 'Estados Unidos' },
+    { codigo: 'MX', nombre: 'México' },
+    { codigo: 'AR', nombre: 'Argentina' },
+    { codigo: 'CO', nombre: 'Colombia' },
+    { codigo: 'CR', nombre: 'Costa Rica' },
+    { codigo: 'ES', nombre: 'España' },
+    { codigo: 'PE', nombre: 'Perú' }
+]
+
+const emit = defineEmits(["obtener-clima"])
+
+const error = ref('')
+
+const busqueda = reactive({
+    ciudad: "",
+    pais: ""
+})
+
+const consultarClima = () => {
+    if(Object.values(busqueda).includes('')){
+        error.value = 'Todos los campos son obligatorios'
+        return
+    }
+
+    error.value = ""
+    emit('obtener-clima', busqueda)
+}
+</script>
+
+<template>
+    <form class="formulario" @submit.prevent="consultarClima">
+        <Alerta v-if="error">{{error}}</Alerta>
+        <div class="campo">
+            <label for="ciudad">Ciudad</label>
+            <input v-model="busqueda.ciudad" type="text" id="ciudad" placeholder="Ciudad">
+        </div>
+        <div class="campo">
+            <label for="pais">País</label>
+            <select v-model="busqueda.pais">
+                <option value="">-- Seleccione un país --</option>
+                <option v-for="pais in paises" :value="pais.codigo">{{pais.nombre}}</option>
+            </select>
+        </div>
+        <input type="submit" value="Consltar clima">
+    </form>
+</template>
